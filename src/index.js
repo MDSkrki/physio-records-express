cbe86e14c15fa42e003eb54c4820a1df6ef61d17
Mihai Daniel Somkereki <somkerekidev@hotmail.com>
2022-05-20 11:23:47 +0200

    created healthcheck endpoint for testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
-import 'dotenv/config';
+import "dotenv/config";
 
 const app = express();
+
+app.get("/api/echo", (req, res) => {
+  res.json({ api: "Alive" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server lifted at port ${process.env.PORT}`);
