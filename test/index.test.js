ac6fc59889a04cf502ff80bc0c29d8dcc7a0e5ad
Mihai Daniel Somkereki <somkerekidev@hotmail.com>
2022-05-22 14:15:53 +0200

    updated tests to connect and disconnect from database

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,28 @@
 import server from "../src/index.js";
 import supertest from 'supertest';
+import { connect, sequelize } from "../src/config/db.js";
 
 const app = supertest(server);
 
-afterAll(()=>{
+beforeAll(async ()=>{
+    await connect();
+});
+
+afterAll(async ()=>{
     server.close();
+
+    try {
+        await sequelize.close();
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 describe("Connectivity tests", () => {
     console.log('Starting connectivity tests');
+
     test('Server connection', async ()=>{
         const res = await app.get('/api/echo');
         expect(res.body.api).toBe('Alive')
-    })
+    });
 });
