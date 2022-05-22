6a0f489b35e326b08f06fc1b8ee74a6629a22711
Mihai Daniel Somkereki <somkerekidev@hotmail.com>
2022-05-22 19:45:13 +0200

    cleaned console logs from tests

--- test/index.test.js
+++ test/index.test.js
@@ -4,10 +4,7 @@ import supertest from 'supertest';
 const app = supertest(server);
 
 describe("Connectivity tests", () => {
-    console.log('Starting connectivity tests');
-
     test('Server connection', async ()=>{
-        console.log('Testing api health');
         const res = await app.get('/api/echo');
         expect(res.body.api).toBe('Alive')
     });
