3796533b556f2162ec13b6e4e5ef6cc2b09d7079
Mihai Daniel Somkereki <somkerekidev@hotmail.com>
2022-05-23 12:04:44 +0200

    updated controller in order to create user at the /register endpoint

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,6 +1,25 @@
 import logger from "../config/logger.js"
+import { User } from "../config/loader.js";
 
-export const register = (req, res) => {
-    logger.info('Register accessed');
-    res.json("Register accessed");
+export const register = async (req, res) => {
+    try {
+        logger.info('Register accessed');
+        console.log(req.body);
+
+        const userData = {
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: req.body.password,
+            phone_number: req.body.phone_number, 
+        }
+    
+        const user = await User.create(userData);
+    
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+        logger.error(error);
+        res.json(error);
+    }
 }
