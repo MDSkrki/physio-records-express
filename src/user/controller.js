82e1631324afe53cd316670fe198389629a74ded
Mihai Daniel Somkereki <somkerekidev@hotmail.com>
2022-05-31 22:16:22 +0200

    added background_information field in controller and tests because i ofrgot and formatted code (auto-format on save)

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,26 +1,27 @@
-import logger from "../config/logger.js"
+import logger from "../config/logger.js";
 import { User } from "../shared/loader.js";
 import { hasher } from "../shared/services/encryption.js";
 
 export const register = async (req, res) => {
-    try {
-        logger.info('Register accessed');
-        console.log(req.body);
+  try {
+    logger.info("Register accessed");
+    console.log(req.body);
 
-        const userData = {
-            name: req.body.name,
-            surname: req.body.surname,
-            email: req.body.email,
-            password: await hasher(req.body.password),
-            phone_number: req.body.phone_number, 
-        }
-    
-        const user = await User.create(userData);
-    
-        res.json(user);
-    } catch (error) {
-        console.log(error);
-        logger.error(error);
-        res.json(error);
-    }
-}
+    const userData = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: await hasher(req.body.password),
+      phone_number: req.body.phone_number,
+      background_information: req.body.background_information,
+    };
+
+    const user = await User.create(userData);
+
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    logger.error(error);
+    res.json(error);
+  }
+};
